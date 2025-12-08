2536ada9087095b940c0a9a8b47f1a235b27942b
Lâm Hoàng Quốc <hoangquoc2106@gmail.com>
2025-12-08 15:20:57 +0700

    Sử dụng bộ lệnh riêng của Honeywell PR3 để xử lý in chữ

--- modules/printer-drivers/android/src/main/java/expo/modules/printerdrivers/drivers/HoneywellPR3Driver.kt
+++ modules/printer-drivers/android/src/main/java/expo/modules/printerdrivers/drivers/HoneywellPR3Driver.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import com.facebook.react.bridge.ReadableMap
 import expo.modules.printerdrivers.bluetoothService.BluetoothService
 import expo.modules.printerdrivers.utils.constants.EscPosCommand
+import expo.modules.printerdrivers.utils.constants.PR3Command
 import expo.modules.printerdrivers.utils.helpers.CommonHelper
 
 class HoneywellPR3Driver(bluetoothService: BluetoothService, context: Context) :
@@ -12,28 +13,43 @@ class HoneywellPR3Driver(bluetoothService: BluetoothService, context: Context) :
     override var printerPageWidth: Int = 52
 
     override fun initPrinter() {
-        buffer.put(EscPosCommand.INIT)
-        // Add this for better special character support
-        buffer.put(EscPosCommand.CODE_PAGE_CP437)
+        buffer.put(PR3Command.INIT)
     }
 
     override fun giayBaoTienNuocNongThon(jsonData: ReadableMap) {
         initPrinter()
 
         buffer.put("---------------------------------------.\n".toByteArray())
-//        buffer.put(EscPosCommand.ALIGN_CENTER)
-//        buffer.put("The quick brown fox jumps over the lazy dog.\n".toByteArray())
-//        buffer.put(EscPosCommand.ALIGN_RIGHT)
-        buffer.put(
-            CommonHelper.wordWrapStr(
-                "Gã vội vã bước nhanh qua phố xá, dưới bóng trời chớm nở những giấc mơ.\n",
-                printerPageWidth
-            ).toByteArray()
-        )
-//        buffer.put(EscPosCommand.BOLD_ON)
+
+//        buffer.put(
+//            CommonHelper.wordWrapStr(
+//                "Gã vội vã bước nhanh qua phố xá, dưới bóng trời chớm nở những giấc mơ.\n",
+//                printerPageWidth
+//            ).toByteArray()
+//        )
+
+        buffer.put(PR3Command.BOLD_ON)
+        buffer.put("Dòng chữ đậm\n".toByteArray())
+        buffer.put(PR3Command.BOLD_OFF)
+
+        buffer.put(PR3Command.DOUBLE_WIDE_ON)
+        buffer.put("Dòng chữ gấp đôi chiều rộng\n".toByteArray())
+        buffer.put(PR3Command.DOUBLE_WIDE_OFF)
+
+        buffer.put(PR3Command.DOUBLE_HIGH_ON)
+        buffer.put("Dòng chữ gấp đôi chiều cao\n".toByteArray())
+        buffer.put(PR3Command.DOUBLE_HIGH_OFF)
+
+        buffer.put(PR3Command.DOUBLE_WH_ON)
+        buffer.put("Dòng chữ gấp đôi chiều cao và chiều rộng\n".toByteArray())
+        buffer.put(PR3Command.DOUBLE_WH_OFF)
+
+        buffer.put("Dòng chữ bình thường\n".toByteArray())
+
+        
         buffer.put("---------------------------------------.\n".toByteArray())
 
-        buffer.put(EscPosCommand.FEED_LINES(2))
-        buffer.put(EscPosCommand.CUT_FULL)
+        buffer.put(PR3Command.NEW_LINE)
+        buffer.put(PR3Command.NEW_LINE)
     }
 }
---
+++ modules/printer-drivers/android/src/main/java/expo/modules/printerdrivers/utils/constants/PR3Command.kt
@@ -0,0 +1,35 @@
+package expo.modules.printerdrivers.utils.constants
+
+object PR3Command {
+    private val ESC = 0x1b.toByte() // ESC
+    private val EM = 0x21.toByte() // ! (Exclamation Mark)
+    private val NLL = 0x00.toByte() // NULL
+    private val CR = 0x0d.toByte() // Carriage Return
+    
+    val INIT = byteArrayOf(
+        NLL, NLL, NLL, NLL,
+        ESC, 0x40,
+        NLL, NLL, NLL
+    )
+
+    val NORMAL_FONT = byteArrayOf(
+        ESC, 0x77, EM,
+        ESC, EM, NLL
+    )
+
+    val BOLD_ON = byteArrayOf(ESC, 0x77, 0x6D)
+    val BOLD_OFF = byteArrayOf(ESC, 0x77, EM)
+
+    val DOUBLE_WIDE_ON = byteArrayOf(ESC, EM, 0x20)
+    val DOUBLE_WIDE_OFF = byteArrayOf(ESC, EM, NLL)
+
+    val DOUBLE_HIGH_ON = byteArrayOf(ESC, EM, 0x10)
+    val DOUBLE_HIGH_OFF = byteArrayOf(ESC, EM, NLL)
+
+    val DOUBLE_WH_ON = byteArrayOf(ESC, EM, 0x30)
+    val DOUBLE_WH_OFF = byteArrayOf(ESC, EM, NLL)
+
+    val NEW_LINE = byteArrayOf(CR)
+
+    val FORM_FEED = byteArrayOf(CR, CR, CR, CR, CR) // = 5 NEW_LINE
+}
